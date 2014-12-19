c116f4b769583bcb8f1143b5cd9bb816204e572e
David Brokaw <dgbrokaw@indiana.edu>
2014-12-19 16:48:50 -0500

    enlarged size of board; made rocks uniform in shape, size are multiples

--- settings.js
+++ settings.js
@@ -1,28 +1,31 @@
-var boardHeight = 500
-   ,boardWidth = 1000
+var scale = 1
+
+   // board stats
+   ,boardHeight = 750*scale
+   ,boardWidth = 1500*scale
    ,boardColor = 'white'
 
-   ,buttonZoneHeight = boardHeight/10
-   ,buttonZoneWidth = boardWidth*6/10
-   ,buttonZoneX = boardWidth*3/40
-   ,buttonZoneY = 25
+   // rock zone stats
+   ,controlRockZoneX = boardHeight/15 // = 50
+   ,controlRockZoneY = controlRockZoneX
 
-   ,buttonHeight = buttonZoneHeight*3/4
-   ,buttonWidth = buttonZoneWidth*1/4
+   ,rockZoneHeight = boardHeight - controlRockZoneX*4
+   ,rockZoneWidth = rockZoneHeight
+
+   ,userRockZoneX = controlRockZoneX + rockZoneWidth + controlRockZoneX
+   ,userRockZoneY = controlRockZoneY
+
+   ,rockZoneColor = 'linen'
+
+   ,correctColor = 'rgb(0,255,0)'
+   ,incorrectColor = 'rgb(255,0,0)'
+
+   // button stats
+   ,buttonHeight = boardHeight/10*3/4//= buttonZoneHeight*3/4
+   ,buttonWidth = boardWidth*6/10*1/4//= buttonZoneWidth*1/4
 
    ,buttonColor = 'rgb(150,150,150)'
    ,buttonPushedColor = 'rgb(100,100,100)'
-
-   ,colorButtonX = buttonZoneWidth*1/16
-   ,colorButtonY = (buttonZoneHeight-buttonHeight)/2
-
-   ,shapeButtonX = colorButtonX + buttonWidth + buttonZoneWidth*1/16
-   ,shapeButtonY = colorButtonY
-
-   ,positionButtonX = shapeButtonX + buttonWidth + buttonZoneWidth*1/16
-   ,positionButtonY = colorButtonY
-
-   ,currentControlMode = 'color'
 
    ,refreshButtonX = boardWidth*1/3
    ,refreshButtonY = boardHeight-buttonHeight
@@ -30,36 +33,37 @@ var boardHeight = 500
    ,nextButtonX = boardWidth*1/2
    ,nextButtonY = boardHeight-buttonHeight
 
-   ,rockZoneColor = 'linen'
-
-   ,rockZoneHeight = 350
-   ,rockZoneWidth = rockZoneHeight
-
-   ,controlRockZoneX = 25
-   ,controlRockZoneY = buttonZoneY + buttonZoneHeight + 25
-
-   ,userRockZoneX = controlRockZoneX + rockZoneWidth + controlRockZoneX
-   ,userRockZoneY = controlRockZoneY
-
-	,correctColor = 'rgb(0,255,0)'
-   ,incorrectColor = 'rgb(255,0,0)'
-
+   // rock stats
    ,rockColors = ['rgb(60,25,25)'
                  ,'rgb(150,155,150)'
                  ,'rgb(225,225,255)']
 
-   ,rockHeight = 30
-
-   ,smallRockHeight = rockHeight
+   ,smallRockHeight = 30
    ,smallRockWidth = smallRockHeight
 
-   ,mediumRockHeight = rockHeight
-   ,mediumRockWidth = 45
+   ,mediumRockHeight = smallRockHeight*2
+   ,mediumRockWidth = mediumRockHeight
 
-   ,largeRockHeight = rockHeight
-   ,largeRockWidth = 70
+   ,largeRockHeight = smallRockHeight*3
+   ,largeRockWidth = largeRockHeight
 
    ,resizeCircleRadius = 5
 
    ,rockDrag
    ,resizeToolDrag
+
+   // ,buttonZoneHeight = boardHeight/10
+   // ,buttonZoneWidth = boardWidth*6/10
+   // ,buttonZoneX = boardWidth*3/40
+   // ,buttonZoneY = 25
+
+   // ,colorButtonX = buttonZoneWidth*1/16
+   // ,colorButtonY = (buttonZoneHeight-buttonHeight)/2
+
+   // ,shapeButtonX = colorButtonX + buttonWidth + buttonZoneWidth*1/16
+   // ,shapeButtonY = colorButtonY
+
+   // ,positionButtonX = shapeButtonX + buttonWidth + buttonZoneWidth*1/16
+   // ,positionButtonY = colorButtonY
+
+   // ,currentControlMode = 'color'
