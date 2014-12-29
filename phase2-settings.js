5a6f32062c4c69f61335210c8ad96d50f8326ba7
David Brokaw <dgbrokaw@indiana.edu>
2014-12-29 16:31:22 -0500

    added phase 2 interface

---
+++ phase2-settings.js
@@ -0,0 +1,11 @@
+var testRockZoneX = (boardWidth-rockZoneWidth)/2
+   ,testRockZoneY = controlRockZoneY
+
+   ,noButtonCX = boardWidth/3
+   ,noButtonCY = refreshButtonCY
+
+   ,yesButtonCX = boardWidth*2/3
+   ,yesButtonCY = nextButtonCY
+
+   ,noColor = incorrectColor
+   ,yesColor = correctColor
